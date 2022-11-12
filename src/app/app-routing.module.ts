18e89d0a2442123b512700b71855a3c94220c7c5
Halil Bilgil <hbilgil@gmail.com>
2022-11-12 15:22:48 +0300

    Edit routing module file to add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "Home", component: HomeComponent},
+  {path: "Shop", component: ShopComponent},
+  {path: "About", component: AboutComponent},
+  {path: "Shopping-Cart", component: ShoppingCartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
