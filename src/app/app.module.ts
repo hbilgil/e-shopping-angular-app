e5254256780de850f85672c64309e62d37aac605
Halil Bilgil <hbilgil@gmail.com>
2022-11-12 15:22:25 +0300

    Edit app module file to add new component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { defineElement } from 'lord-icon-element';
 import lottie from 'lottie-web';
+
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContainerComponent } from './container/container.component';
 import { HomeComponent } from './home/home.component';
 import { HomeDiv1Component } from './home-div1/home-div1.component';
 import { HomeDiv2Component } from './home-div2/home-div2.component';
+import { ShopComponent } from './shop/shop.component';
+import { AboutComponent } from './about/about.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CardComponent } from './card/card.component';
+import { AboutDiv1Component } from './about-div1/about-div1.component';
+import { AboutDiv2Component } from './about-div2/about-div2.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ProductViewComponent } from './product-view/product-view.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +30,16 @@ import { HomeDiv2Component } from './home-div2/home-div2.component';
     ContainerComponent,
     HomeComponent,
     HomeDiv1Component,
-    HomeDiv2Component
+    HomeDiv2Component,
+    ShopComponent,
+    AboutComponent,
+    ShoppingCartComponent,
+    CategoriesComponent,
+    CardComponent,
+    AboutDiv1Component,
+    AboutDiv2Component,
+    ErrorPageComponent,
+    ProductViewComponent
   ],
   imports: [
     BrowserModule,
