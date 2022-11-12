bf9a2585e0c12b1d751846acef67bec89bf415b9
Halil Bilgil <hbilgil@gmail.com>
2022-11-12 21:57:43 +0300

    Edit app routing module file to add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ErrorNotFoundComponent } from './error-not-found/error-not-found.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
-  {path: "", redirectTo:"Home", pathMatch:"full"},
+  //{path: "", redirectTo:"Home", pathMatch:"full"},
+  {path: "", component: HomeComponent},
   {path: "Home", component: HomeComponent},
   {path: "Shop", component: ShopComponent},
   {path: "About", component: AboutComponent},
   {path: "ShoppingCart", component: ShoppingCartComponent},
+  {path: "ErrorPage", component: ErrorPageComponent},
   {path: "**", component: ErrorNotFoundComponent}
 ];
 
