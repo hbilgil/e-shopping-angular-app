442a9b27d81f967c03a65a74443f8a2347d168cb
Halil Bilgil <hbilgil@gmail.com>
2022-11-12 17:10:23 +0300

    Edit rouing module file to define new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ErrorNotFoundComponent } from './error-not-found/error-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
+  {path: "", redirectTo:"Home", pathMatch:"full"},
   {path: "Home", component: HomeComponent},
   {path: "Shop", component: ShopComponent},
   {path: "About", component: AboutComponent},
-  {path: "Shopping-Cart", component: ShoppingCartComponent}
+  {path: "ShoppingCart", component: ShoppingCartComponent},
+  {path: "**", component: ErrorNotFoundComponent}
 ];
 
 @NgModule({
