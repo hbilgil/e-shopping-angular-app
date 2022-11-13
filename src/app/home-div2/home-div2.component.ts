7f9111f8d685384f7fe896725c5f137c8961ab24
Halil Bilgil <hbilgil@gmail.com>
2022-11-13 23:54:03 +0300

    Edit home-div2 to migrate product data

--- src/app/home-div2/home-div2.component.ts
+++ src/app/home-div2/home-div2.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../Services/products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home-div2',
   templateUrl: './home-div2.component.html',
-  styleUrls: ['./home-div2.component.css']
+  styleUrls: ['./home-div2.component.css'],
+  providers: [
+    ProductsService
+  ]
 })
 export class HomeDiv2Component implements OnInit {
+  products: any;
+  product: any;
+  productId: any;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private service: ProductsService) { }
 
   ngOnInit(): void {
+    this.products = this.service.products;
+    this.productId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.product = this.service.products.find((element) => element.id == this.productId);
   }
 
 }
