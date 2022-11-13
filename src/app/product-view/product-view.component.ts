69862c587aabcf9d20af83adbbe76b44423a10de
Halil Bilgil <hbilgil@gmail.com>
2022-11-13 22:16:33 +0300

    Edit product view component to declare product data

--- src/app/product-view/product-view.component.ts
+++ src/app/product-view/product-view.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../Services/products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
-  styleUrls: ['./product-view.component.css']
+  styleUrls: ['./product-view.component.css'],
+  providers: [
+    ProductsService
+  ]
 })
-export class ProductViewComponent implements OnInit {
 
-  constructor() { }
+export class ProductViewComponent implements OnInit  {
+  
+  product: any;
+  productId: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private service: ProductsService) { }
 
   ngOnInit(): void {
+    this.productId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.product = this.service.products.find((element) => element.id == this.productId);
   }
 
 }
