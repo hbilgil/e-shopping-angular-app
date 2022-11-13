5b3a1b0d9123a00e891040be7103d3e80517f047
Halil Bilgil <hbilgil@gmail.com>
2022-11-13 22:14:04 +0300

    Edit shopping cart file for data migration

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../Services/products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  styleUrls: ['./shopping-cart.component.css'],
+  providers: [
+    ProductsService
+  ]
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  product: any;
+  productId: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private service: ProductsService) { }
 
   ngOnInit(): void {
+    this.productId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.product = this.service.products.find((element) => element.id == this.productId);
   }
 
 }
