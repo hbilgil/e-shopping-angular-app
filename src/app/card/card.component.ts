14004829426b35a47f125da2e8ac8fcee6fc5cf1
Halil Bilgil <hbilgil@gmail.com>
2022-11-13 22:18:21 +0300

    Edit card component file to declare product data

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../Services/products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
+  providers: [
+    ProductsService
+  ]
 })
 export class CardComponent implements OnInit {
+  products: any;
+  product: any;
+  productId: any;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private service: ProductsService) { }
 
   ngOnInit(): void {
+    this.products = this.service.products;
+    this.productId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.product = this.service.products.find((element) => element.id == this.productId);
   }
-
 }
