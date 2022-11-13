2741dc5fe05b64fe8654f8b9e1431fc1c91781be
Halil Bilgil <hbilgil@gmail.com>
2022-11-13 22:13:15 +0300

    Edit shopping cart file for data migration

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../Services/products.service';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.css']
+  styleUrls: ['./shop.component.css'],
+  providers: [
+    ProductsService
+  ]
 })
 export class ShopComponent implements OnInit {
 
-  constructor() { }
+  products: any;
+
+  constructor(private service: ProductsService) { }
 
   ngOnInit(): void {
+    this.products = this.service.products;
   }
 
 }
