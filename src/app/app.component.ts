0037ad8a77d18e40cefc91d3947270a8aa200da6
Halil Bilgil <hbilgil@gmail.com>
2022-11-13 22:20:57 +0300

    Edit app component to declare product data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './Services/products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductsService]
 })
-export class AppComponent {
-  title = 'e-shopping-angular-app';
+export class AppComponent implements OnInit {
+  title = "e-shopping-angular-app";
+  product: any;
+  productId: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private service: ProductsService) { }
+
+  ngOnInit(): void {
+    this.productId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.product = this.service.products.find((element) => element.id == this.productId);
+  }
 }
