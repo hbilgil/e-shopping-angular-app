015a23cac44ff3e432049aad9d772925ea10ee95
Halil Bilgil <hbilgil@gmail.com>
2022-11-14 22:01:46 +0300

    Edit product view component file to add new variables

--- src/app/product-view/product-view.component.ts
+++ src/app/product-view/product-view.component.ts
@@ -15,12 +15,21 @@ export class ProductViewComponent implements OnInit  {
   
   product: any;
   productId: any;
+  stockTotal: any;
+  stockS: any;
+  stockM: any;
+  stockL: any;
+  stockXL: any;
 
   constructor(private activatedRoute: ActivatedRoute, private service: ProductsService) { }
 
   ngOnInit(): void {
     this.productId = this.activatedRoute.snapshot.paramMap.get('id');
     this.product = this.service.products.find((element) => element.id == this.productId);
-  }
-
+    this.stockS = this.product.stock.S;
+    this.stockM = this.product.stock.M;
+    this.stockL = this.product.stock.L;
+    this.stockXL = this.product.stock.XL
+    this.stockTotal = this.stockS + this.stockM + this.stockL + this.stockXL
+    }
 }
