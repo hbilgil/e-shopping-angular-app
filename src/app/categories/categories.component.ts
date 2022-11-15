5a170b2652e2f9708090314baaf5fe53d25951d6
Halil Bilgil <hbilgil@gmail.com>
2022-11-15 20:40:26 +0300

    Edit categories files to declare new variables methods and elements

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
@@ -11,5 +11,20 @@ export class CategoriesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  selectedGenderButtonValue: string = "Men";
+  selectedCategoryButtonValue: string = "T-shirts";
+
+  @Output() genderFilterButtonSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
+  @Output() categoryFilterButtonSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  onButtonSelectionGenderChanged() {
+    this.genderFilterButtonSelectionChanged.emit(this.selectedGenderButtonValue);
+  }
+
+  onButtonSelectionCategoryChanged() {
+    this.categoryFilterButtonSelectionChanged.emit(this.selectedCategoryButtonValue);
+  }
+  
 
 }
