2275ed6a20d49748658939812c35ca848828c3cc
Halil Bilgil <hbilgil@gmail.com>
2022-11-19 00:07:19 +0300

    Edit shopping cart for new functions, elements and styling

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,25 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductsService } from '../Services/products.service';
-import { ActivatedRoute } from '@angular/router';
+import { ChosenItemsService } from '../Services/chosenItems.service';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css'],
-  providers: [
-    ProductsService
-  ]
 })
 export class ShoppingCartComponent implements OnInit {
+  chosenItems: any;
+  totalQuantity: any;
+  totalPrice: number = this.service.totalPrice;
+  shippingPrice: number = 20;
 
-  product: any;
-  productId: any;
-
-  constructor(private activatedRoute: ActivatedRoute, private service: ProductsService) { }
+  constructor(private service: ChosenItemsService) { }
 
   ngOnInit(): void {
-    this.productId = this.activatedRoute.snapshot.paramMap.get('id');
-    this.product = this.service.products.find((element) => element.id == this.productId);
+    this.chosenItems = this.service.items;
+    this.totalQuantity = this.service.totalQuantity;
   }
 
+  removeItem(item: any) {
+    this.service.removeItem(item);
+  }
 }
