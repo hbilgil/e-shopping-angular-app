79735eb43b085bd330204423312f4d2b032ecc5a
Halil Bilgil <hbilgil@gmail.com>
2022-11-20 00:42:09 +0300

    Edit chosenItems service data to add new functions

--- src/app/Services/chosenItems.service.ts
+++ src/app/Services/chosenItems.service.ts
@@ -10,6 +10,49 @@ export class ChosenItemsService {
             if (x.name === item.name && x.size === item.size) {
                 x.quantity += 1;
                 isInCart = true;
+                switch(x.size) {
+                    case 'S':
+                        x.stockA.S--;
+                    break;
+                    case 'M':
+                        x.stockA.M--;
+                    break;
+                    case 'L':
+                        x.stockA.L--;
+                    break;
+                    case 'XL':
+                        x.stockA.XL--;
+                    break;
+                    case '37':
+                        x.stockB.num1--;
+                    break;
+                    case '38':
+                        x.stockB.num2--;
+                    break;
+                    case '39':
+                        x.stockB.num3--;
+                    break;
+                    case '40':
+                        x.stockB.num4--;
+                    break;
+                    case '41':
+                        x.stockB.num5--;
+                    break;
+                    case '42':
+                        x.stockB.num6--;
+                    break;
+                    case '43':
+                        x.stockB.num7--;
+                      break;
+                    case '44':
+                        x.stockB.num8--;
+                    break;
+                    case '45':
+                        x.stockB.num9--;
+                    break;
+                    case '46':
+                        x.stockB.num10--;
+                }
             }
         });
         if (!isInCart) {
@@ -25,5 +68,19 @@ export class ChosenItemsService {
                 this.items.splice(index, 1);
             }
         })
+        this.totalQuantity.splice(this.totalQuantity.length-item.quantity, item.quantity);
+        this.totalPrice -= (item.quantity)*(item.price);
+    }
+
+    decreaseQuantity(item: any) {
+        item.quantity -= 1;
+        this.totalQuantity.pop();
+        this.totalPrice -= item.price;
+    }
+
+    increaseQuantity(item: any) {
+        item.quantity += 1;
+        this.totalQuantity.push('x');
+        this.totalPrice += item.price;    
     }
 }
