d62a65db4fbec1798ec018ed8e84629de3822ce4
Halil Bilgil <hbilgil@gmail.com>
2022-11-21 21:19:30 +0300

    Add sidebar component for account page control

--- src/app/account/sidebar/sidebar.component.ts
+++ src/app/account/sidebar/sidebar.component.ts
@@ -1,23 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
-  styles: [`
-      :host ::ng-deep button {
-          margin-right: .25em;
-      }
-  `]
 })
 export class SidebarComponent implements OnInit {
-  visibleSidebar1: any = true;
 
-  constructor(private primengConfig: PrimeNGConfig) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.primengConfig.ripple = true;
   }
 
 }
