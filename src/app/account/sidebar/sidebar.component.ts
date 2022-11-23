dd8e37d5fa041e9d34fd23b3a245c026395587a1
Halil Bilgil <hbilgil@gmail.com>
2022-11-23 12:31:43 +0300

    Edit sidebar component for routing changes

--- src/app/account/sidebar/sidebar.component.ts
+++ src/app/account/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  logOut() {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You will be logged out!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, please!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Logged out!',
+          'Thank you for visiting us',
+          'success'
+        )
+        this.authService.SignOut();
+      }
+  })
   }
 
 }
