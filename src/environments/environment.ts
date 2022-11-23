5cbe8c941fb438fb951d082649a7f09ba38aa611
Halil Bilgil <hbilgil@gmail.com>
2022-11-23 12:27:26 +0300

    Add firebase credentials for authentication sync

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,23 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+production: false,
+firebase: {
 
+  apiKey: "AIzaSyD-YAgMzwLA48-SqS3PeQmesRgMVFUlJDk",
+
+  authDomain: "e-commerce-auth-ad075.firebaseapp.com",
+
+  projectId: "e-commerce-auth-ad075",
+
+  storageBucket: "e-commerce-auth-ad075.appspot.com",
+
+  messagingSenderId: "648650141748",
+
+  appId: "1:648650141748:web:0490dd6e255ac56eecda61"
+
+}
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
