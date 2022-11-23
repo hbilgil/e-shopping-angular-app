8b6ff57448cce36f67ca05ef171c07354456f96a
Halil Bilgil <hbilgil@gmail.com>
2022-11-24 00:44:39 +0300

    Add models file

---
+++ src/app/Models/chosenItem.model.ts
@@ -0,0 +1,26 @@
+export interface ChosenProduct {
+    uniqId: string;
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+    size: string;
+    quantity: number;
+    stockA: {
+      S: number;
+      M: number;
+      L: number;
+      XL: number;};
+    stockB: {
+      num1: number;
+      num2: number;
+      num3: number;
+      num4: number;
+      num5: number;
+      num6: number;
+      num7: number;
+      num8: number;
+      num9: number;
+      num10: number;
+    };
+};
