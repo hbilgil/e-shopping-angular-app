94d1f8198ae2c82984b4dcb31e4648a9e8e3f854
Halil Bilgil <hbilgil@gmail.com>
2022-11-24 14:24:10 +0300

    Add shortenText pipe for details section at card component

---
+++ src/app/Services/shortenText.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shortenText'
+})
+export class ShortenTextPipe implements PipeTransform {
+
+  transform(text: any, length: number = 20, showAll: boolean = false, suffix: string = '...'): any {
+    
+    if (showAll) {
+      return text;
+    }
+
+    if ( text.split(" ").length > length ) {
+      
+      return text.split(" ").splice(0, length).join(" ") + suffix;
+    }
+
+    return text;
+  }
+}
