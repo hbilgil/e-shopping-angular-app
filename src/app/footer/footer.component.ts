050f0ce968f195ba030c0df29d6bca772e839386
Halil Bilgil <hbilgil@gmail.com>
2022-11-25 16:18:28 +0300

    Edit footer files to add functionality for newsletter button

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-footer',
@@ -12,4 +13,32 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  enteredSearchValue: string = ''; //empty string value is automatically assigned in start
+
+  validateEmail (email: any) { //a function to return whether the email is valid or not..
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  }
+  
+  subscribeToNewsletter() {// a function to subscribe to the weekly newsletter
+    if(this.validateEmail(this.enteredSearchValue)) { //if the provided email is valid
+      Swal.fire({ //an async function provided by an imported file
+        position: 'top-end',
+        icon: 'success',
+        title: `Hi, </br> We will sent our weekly newsletter to ${this.enteredSearchValue}!`,
+        showConfirmButton: false,
+        timer: 1500
+      })
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: 'Please provide a valid e-mail address!'
+      })
+    }
+  }
 }
