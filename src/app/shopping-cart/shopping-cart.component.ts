51fc4c28d22e07c472cf1b2747d0493d47b9e9ae
Halil Bilgil <hbilgil@gmail.com>
2022-11-27 00:29:30 +0300

    Edit shopping cart for localstorage

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -11,10 +11,13 @@ import Swal from 'sweetalert2/dist/sweetalert2.js';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  chosenItems: any;
-  orderedItems: any;
-  totalQuantity: any;
-  totalPrice: number = this.service2.totalPrice;
+  LOCAL_STORAGE_CHOSEN_ITEMS_LIST: string = 'chosen-items.list' //all ordered items are kept in local store
+  LOCAL_STORAGE_TOTAL_QUANTITY: string = 'totalQuantity' //all ordered items are kept in local store
+  LOCAL_STORAGE_TOTAL_PRICE: string = 'totalPrice' //all ordered items are kept in local store
+
+  chosenItems: any = JSON.parse(localStorage.getItem(this.LOCAL_STORAGE_CHOSEN_ITEMS_LIST));
+  totalQuantity: any = JSON.parse(localStorage.getItem(this.LOCAL_STORAGE_TOTAL_QUANTITY));
+  totalPrice: number = JSON.parse(localStorage.getItem(this.LOCAL_STORAGE_TOTAL_PRICE));
   shippingPrice: number = 20;
 
   constructor(private service: ProductsService, private service2: ChosenItemsService, private service3: OrderedItems) {
@@ -23,7 +26,7 @@ export class ShoppingCartComponent implements OnInit {
   ngOnInit(): void {
     this.chosenItems = this.service2.items;//chosen items are imported from chosenItems service data
     this.totalQuantity = this.service2.totalQuantity;//total quantity is imported from chosenItems service data
-    this.orderedItems = this.service3.orderedItems;//ordered items are imported from orderedItems service data
+    this.totalPrice = this.service2.totalPrice;
   }
 
   addItemsToOrderedItems() {//a function to add chosen Items to Ordered Items in account
@@ -35,6 +38,7 @@ export class ShoppingCartComponent implements OnInit {
     this.service3.addOrderedItemToAccount(this.chosenItems)//a function to be called back from orderedItems service data
     this.service2.removeAllItems(this.chosenItems)//a function to be called back from chosenItems service data
     this.totalPrice = 0;
+    this.saveInLocalStorageData();
   }
 
   removeItem(item: any) {//a function to remove an item from shopping cart
@@ -56,6 +60,7 @@ export class ShoppingCartComponent implements OnInit {
         this.service2.removeItem(item);//a function to be called back from chosenItems service data
         this.service.addItemQuantityToStock(item);//a function to be called back from products service data
         this.totalPrice -= (item.quantity)*(item.price);
+        this.saveInLocalStorageData();
         }
     })
   }
@@ -107,6 +112,7 @@ export class ShoppingCartComponent implements OnInit {
               item.stockB.num10++;
       }
     this.totalPrice -= item.price;
+    this.saveInLocalStorageData();
   }
 
   increaseQuantity(item: any, itemSize: any) {//a function to increase quantity of a chosen Item
@@ -156,5 +162,12 @@ export class ShoppingCartComponent implements OnInit {
           item.stockB.num10--;
   }
     this.totalPrice += item.price;
+    this.saveInLocalStorageData();
+  }
+
+  saveInLocalStorageData() {
+    localStorage.setItem(this.LOCAL_STORAGE_CHOSEN_ITEMS_LIST, JSON.stringify(this.chosenItems))
+    localStorage.setItem(this.LOCAL_STORAGE_TOTAL_QUANTITY, JSON.stringify(this.totalQuantity))
+    localStorage.setItem(this.LOCAL_STORAGE_TOTAL_PRICE, JSON.stringify(this.totalPrice))
   }
 }
