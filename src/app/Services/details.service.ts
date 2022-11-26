6d4e08a6266b68c760998484ccfb7ccda88233f5
Halil Bilgil <hbilgil@gmail.com>
2022-11-27 00:32:43 +0300

    Edit details service to add injectable directive

--- src/app/Services/details.service.ts
+++ src/app/Services/details.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
 export class DetailsService {//a service data to be used in components for product details
     details = [
         {   id: 1,
